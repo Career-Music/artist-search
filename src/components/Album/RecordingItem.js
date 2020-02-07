4e062c56cb48e87675da626295116f1c7d0c258b
Ben Beekman <bbeekman@gmail.com>
2020-02-06 17:56:42 -0800

    recordingItem fix

--- src/components/Album/RecordingItem.js
+++ src/components/Album/RecordingItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 const RecordingItem = ({ title, artistName }) => {
 
   return (
     <>
       <Link to={ `/lyrics/${artistName}/${title}` }>
-        <li>{ title }</li>
+        <li>{ title.title }</li>
       </Link>
     </>
   );
