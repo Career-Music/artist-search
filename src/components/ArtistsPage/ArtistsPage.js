e5a1b9400abdea7ed31c6f5b9c950013180f7c83
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-07 14:38:44 -0800

    got search page working

--- src/components/ArtistsPage/ArtistsPage.js
+++ src/components/ArtistsPage/ArtistsPage.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArtistList from './ArtistList';
 import Header from '../Header/Header';
+import useArtists from '../../hooks/use-artists';
+import { useParams, useHistory } from 'react-router-dom';
 
 const ArtistsPage = () => {
+  const [searchText, setSearchText] = useState('');
+  const { artists, fetchArtists } = useArtists([]);
+  const history = useHistory();
+  const { artistName } = useParams();
+
+  if(artistName && artistName.length > 0) {
+    fetchArtists(artistName);
+  }
+
+  const handleSearchClick = (text) => {
+    history.push(`/search/${text}`);
+  };
 
   return (
     <>
       <Header title='Artist Search' />
-      <input type="text" placeholder="Search for Artist" />
-      <button>Search</button>
-      <ArtistList />
+      <input type="text" onChange={({ target }) => setSearchText(target.value) } placeholder="Search for Artist" />
+      <button onClick={() => handleSearchClick(searchText)}>Search</button>
+      <ArtistList artists={artists} />
     </>
   );
 };
