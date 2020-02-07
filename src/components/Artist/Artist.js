00fcd03ca1579cf3e6c711575a6419271a841664
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-06 16:52:31 -0800

    renamed albums to artist

--- src/components/Artist/Artist.js
+++ src/components/Artist/Artist.js
@@ -2,12 +2,14 @@ import React from 'react';
 import AlbumItem from './AlbumItem';
 import Header from '../Header/Header';
 
-const Albums = ({ artistId }) => {
-  const albums = useAlbums(artistId);
+const Artist = ({ artist }) => {
+  const albums = useAlbums(artist.id);
   return (
     <>
-      <Header title='Albums' />
+      <Header title={`Albums by ${artist.name}`} />
       { albums.map((album) => <AlbumItem key={ album.id } album={ album } />) }
     </>
   );
 };
+
+export default Artist;
