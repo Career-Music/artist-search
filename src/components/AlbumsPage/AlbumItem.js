a232f9020016b48db8a642762749493a2b32573d
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-09 18:36:36 -0800

    allbum placeholder

--- src/components/AlbumsPage/AlbumItem.js
+++ src/components/AlbumsPage/AlbumItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import placeholder from '../../assets/placeholder.jpg';
 const AlbumItem = ({ artistName, album }) => {
   return (
     <>
       <Link to={ `/artist/${artistName}/album/${album.id}` }>
         <div>
-          <img src={ `http://coverartarchive.org/release/${album.id}/front` } />
+          { album.hasCover ? 
+            <img src={ `http://coverartarchive.org/release/${album.id}/front` } /> : 
+            <img src={ placeholder } /> }
           <p>{ album.title }</p>
           <p>{ album.date }</p>
         </div>
