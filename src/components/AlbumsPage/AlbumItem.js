74640dd77689dfaa7423e0029545409e7adeff5a
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-09 18:48:57 -0800

    oops, it's hasArt

--- src/components/AlbumsPage/AlbumItem.js
+++ src/components/AlbumsPage/AlbumItem.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import placeholder from '../../assets/placeholder.jpg';
 const AlbumItem = ({ artistName, album }) => {
+  console.log(album);
   return (
     <>
       <Link to={ `/artist/${artistName}/album/${album.id}` }>
         <div>
-          { album.hasCover ? 
+          { album.hasArt ? 
             <img src={ `http://coverartarchive.org/release/${album.id}/front` } /> : 
             <img src={ placeholder } /> }
           <p>{ album.title }</p>
